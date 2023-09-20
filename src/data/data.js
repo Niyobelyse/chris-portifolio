8b356c40911a12e9425e492cc5c81facb412afcd
Your Name <you@example.com>
2023-09-20 10:00:56 +0200

    updating my linkedin link

--- src/data/data.js
+++ src/data/data.js
@@ -4,41 +4,40 @@ import calc from '../assets/projects/calculator.png'
 import grw from '../assets/projects/grow2gether.png'
 import flg from '../assets/projects/flags.png'
 
-export const data=[
-    {
-        id:1,
-        name:"E-STORE Application",
-        image:estore,
-        github:"https://github.com/Irakiza44/Ecommerce-webapp",
-        live:"https://irak-ecommerce-app.netlify.app/",
+export const data = [{
+        id: 1,
+        name: "E-STORE Application",
+        image: estore,
+        github: "https://github.com/Irakiza44/Ecommerce-webapp",
+        live: "https://irak-ecommerce-app.netlify.app/",
     },
     {
-        id:2,
-        name:"MOVIES SEARCH Application",
-        image:movie,
-        github:"https://github.com/Irakiza44/movie_universe-app",
-        live:"https://movie-universe-app-seven.vercel.app/",
+        id: 2,
+        name: "MOVIES SEARCH Application",
+        image: movie,
+        github: "https://github.com/Irakiza44/movie_universe-app",
+        live: "https://movie-universe-app-seven.vercel.app/",
     },
     {
-        id:3,
-        name:"CALCULATOR Application",
-        image:calc,
-        github:"https://github.com/Irakiza44/react-calculator",
-        live:"https://react-calculator-cvwdn5w7o-irakiza44.vercel.app/",
+        id: 3,
+        name: "CALCULATOR Application",
+        image: calc,
+        github: "https://github.com/Irakiza44/react-calculator",
+        live: "https://react-calculator-cvwdn5w7o-irakiza44.vercel.app/",
     },
     {
-        id:4,
-        name:"Grow2gether Application",
-        image:grw,
-        github:"https://github.com/Irakiza44/Grow2gether",
-        live:"https://grow2gether-6t2zyp2ub-irakiza44.vercel.app/",
+        id: 4,
+        name: "Grow2gether Application",
+        image: grw,
+        github: "https://github.com/Irakiza44/Grow2gether",
+        live: "https://grow2gether-6t2zyp2ub-irakiza44.vercel.app/",
     },
     {
-        id:5,
-        name:"Rest-countries-api-with-color-theme-switcher",
-        image:flg,
-        github:"https://github.com/Irakiza44/Rest-countries-api-with-color-theme-switcher",
-        live:"",
+        id: 5,
+        name: "Rest-countries-api-with-color-theme-switcher",
+        image: flg,
+        github: "https://github.com/Irakiza44/Rest-countries-api-with-color-theme-switcher",
+        live: "https://chimerical-pixie-67da24.netlify.app",
     },
 
 
